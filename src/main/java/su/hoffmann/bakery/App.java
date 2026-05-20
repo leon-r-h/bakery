3378100349ed1ddf10a20de8b6c5c78398f6e0e4
leon_h_07@icloud.com <leon.hoffmann@siemens.com>
2026-05-20 12:06:53 +0200

    Hello world

---
+++ src/main/java/su/hoffmann/bakery/App.java
@@ -0,0 +1,13 @@
+package su.hoffmann.bakery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
