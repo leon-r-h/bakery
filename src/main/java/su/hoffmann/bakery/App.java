09f1993a366d58726956f9e05afb0c2c745ef994
leon-r-h <leon_h_07@icloud.com>
2026-05-20 12:51:53 +0200

    gwgerre

--- src/main/java/su/hoffmann/bakery/App.java
+++ src/main/java/su/hoffmann/bakery/App.java
@@ -8,8 +8,11 @@ package su.hoffmann.bakery;
 
 public class App 
 {
-    public static void main( String[] args )
+	
+    
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+	
 }
